a5742970366458def84eb860ca3e1018aecda25c
Anuj Kumar Pathak <anuj.pathak@devstringx.com>
2021-11-26 00:50:23 +0530

    add a home work file

---
+++ DailyHomeWork.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * DATE - NOV 26
+ * => Setup git in terminal.
+ * => Clone project from github (branch name : nitish_new).
+ * => Add validation for email, name, password, retype password. 
+ * => Try to use regular express to validate.
+ */
+
+?>
